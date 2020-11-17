fafd13a8ff32fb7db90960d16b6a006726719fbb
SethCommichaux <yofi01@gmail.com>
2020-11-16 19:53:29 -0500

    Update synteny_edit_distance.py

--- synteny_edit_distance.py
+++ synteny_edit_distance.py
@@ -7,14 +7,13 @@ import sys
 def edit_dist(a,b):
     a = map(int,a)
     b = map(int,b)
-    "Calculates the Levenshtein distance between a and b."
     n, m = len(a), len(b)
     if n > m:
-        # Make sure n <= m, to use O(min(n,m)) space
         a,b = b,a
         n,m = m,n
-        
+    
     current = range(n+1)
+    
     for i in range(1,m+1):
         previous, current = current, [i]+[0]*n
         for j in range(1,n+1):
@@ -23,7 +22,7 @@ def edit_dist(a,b):
             if a[j-1] != b[i-1]:
                 change = change + 1
             current[j] = min(add, delete, change)
-            
+    
     return current[n]
 
 
